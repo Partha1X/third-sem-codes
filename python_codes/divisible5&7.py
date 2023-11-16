be7340091a8280a6056c6a0f7329fa6964063c5a
Partha1X <nimbus2kilo@gmail.com>
2023-11-17 03:54:00 +0530

    DKS Extra-divisible by 5 and 7

--- python_codes/divisible5&7.py
+++ python_codes/divisible5&7.py
@@ -1,6 +1,6 @@
 #WAP to print all the Numbers between 1 and 300 which are 'divisible by 5 and 7'
 
-print("The Numbers divisible by 5 and 7 are:")
+print("The Numbers divisible by 5 and 7 are: ")
 for i in range(1, 301):
     if i % 5 == 0 and i % 7 == 0:
         print(i, end=' ')
