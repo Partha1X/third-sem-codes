9ab3a0425b027ea1b7f61ee504535401c65dd4c3
Partha1X <nimbus2kilo@gmail.com>
2023-11-17 04:05:31 +0530

    Calculate s = 1^2 + 2^2 + 3^2 + ... up to n

---
+++ python_codes/Cal1pow2+2pow2.py
@@ -0,0 +1,11 @@
+# Calculate s = 1^2 + 2^2 + 3^2 + ... up to n using math function
+
+
+import math 
+n = int(input("Enter the value of n:"))
+s = 0
+for i in range(1, n+1):
+    s = s + math.pow(i, 2)  # in floating point
+    # s = s + int(math.pow(i, 2))  # in integer
+
+print("Sum =", s)
