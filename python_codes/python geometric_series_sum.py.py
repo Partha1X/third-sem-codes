429f8d0a3d0250360b6df36d23652ed6bd285134
Partha1X <nimbus2kilo@gmail.com>
2023-11-17 18:18:41 +0530

    Geometric Series (Sum)

--- python_codes/python geometric_series_sum.py.py
+++ python_codes/python geometric_series_sum.py.py
@@ -1,4 +1,17 @@
-# Geometric Series (Sum):
+''' Geometric Series (Sum):
+geometric series a+ar+ar^2+ar^3...
+Here , 
+'S' is the sum of the series
+'a' is the first term
+'r' is the common ratio
+We have ,  infinite geometric series also
+'''
+'''
+#The key difference is how the terms in the series are generated:
+
+~In an arithmetic series, each term is obtained by adding a fixed number to the previous term.
+~In a geometric series, each term is obtained by multiplying the previous term by a fixed number.
+'''
 
 def geometric_series_sum(a, r, n):
     return a * (1 - r**n) / (1 - r)
