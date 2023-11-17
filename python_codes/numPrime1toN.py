70c98088377e33e17cd092844cedb6b087dc4ed2
Partha1X <nimbus2kilo@gmail.com>
2023-11-18 00:41:06 +0530

    Write a program to show prime numbers between 1 to 100(PyQ2019)

--- python_codes/numPrime1toN.py
+++ python_codes/numPrime1toN.py
@@ -1,4 +1,26 @@
-#16. WAP to print all Prime Numbers between 1 and n
+#Write a program to show prime numbers between 1 to 100(PyQ2019)
+# Function to check if a number is prime
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Function to print prime numbers in the specified range
+def print_primes_in_range(start, end):
+    for num in range(start, end + 1):
+        if is_prime(num):
+            print(num, end=' ')
+
+# Print prime numbers between 1 and 100
+print_primes_in_range(1, 100)
+
+
+
+
+'''#16. WAP to print all Prime Numbers between 1 and n
 #(numbers having only two factors,1 & itsself)
 
 n = int(input("Enter The Upper Limit: "))
@@ -11,3 +33,4 @@ for i in range(1, n + 1): # Loop through numbers from 1 to n (inclusive)
     if c == 2: # If the counter c is 2, the number i is prime (it has only two factors: 1 and itself)
         print(i, end=' ') # Print the prime number i, end with a space
 
+'''
