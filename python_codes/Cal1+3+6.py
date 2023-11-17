809071d12d3cc012ee6798534b76df288b50dba1
Partha1X <nimbus2kilo@gmail.com>
2023-11-17 13:57:19 +0530

    DKS Extra-Calculate s=1+3+6+10+15+...

---
+++ python_codes/Cal1+3+6.py
@@ -0,0 +1,21 @@
+'''Compute the sum of first n terms of the following series:
+s=1+3+6+10+15+...
+'''
+
+n = int(input("Enter No of terms: "))
+# Initialize variables
+count = 0  # Counter for the number of terms
+s = 0      # Variable to store the sum
+num = 1    # Initial term of the series
+step = 2   # Difference between consecutive terms in the series
+
+# Loop to generate terms and calculate the sum
+while count < n:
+    print(num, end=' ')  # Print the current term
+    s = s + num          # Add the current term to the sum
+    num = num + step     # Update the current term for the next iteration
+    step = step + 1      # Increase the step for the next iteration
+    count = count + 1    # Increment the counter
+
+# Print the final sum after the loop
+print("\nSum =", s)
