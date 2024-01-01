496ff6af66c5e5787b8dea7c1a9b4428da4b3241
Partha1X <nimbus2kilo@gmail.com>
2024-01-02 02:56:27 +0530

    WAP to Create a Linked List

--- data_stracture_codes/LinkedList.c
+++ data_stracture_codes/LinkedList.c
@@ -1,1 +1,57 @@
-//WAP to Create a Linked List 
+// WAP to Create a Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *start = NULL;
+
+void createList()
+{
+    int item, i, n;
+    struct node *tmp, *ptr;
+
+    printf("\nEnter No of Nodes: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("\nEnter Item: ");
+        scanf("%d", &item);
+
+        tmp = (struct node *)malloc(sizeof(struct node));
+        tmp->info = item;
+        tmp->link = NULL;
+
+        if (start == NULL)
+            start = tmp;
+        else
+        {
+            ptr = start;
+            while (ptr->link != NULL)
+                ptr = ptr->link;
+
+            ptr->link = tmp;
+        }
+    }
+
+    ptr = start;
+    printf("The Elements Are: ");
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->info);
+        ptr = ptr->link;
+    }
+
+    printf("NULL");
+}
+
+int main()
+{
+    createList();
+    return 0;
+}
