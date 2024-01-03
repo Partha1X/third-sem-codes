d7b4be26b283b71998fa435acd2e613d5d5851b8
Partha1X <nimbus2kilo@gmail.com>
2024-01-03 21:12:55 +0530

    WAP to Check Whether a Given Number is Armstrong or Not

---
+++ OS_Shell_Programs/ArmstrongNumber.sh
@@ -0,0 +1,18 @@
+# WAP to Check Whether a Given Number is Armstrong or Not
+
+echo -n "Enter a Number: "
+read n
+s=0
+temp=$n
+
+while ((n!=0)); do
+    r=$((n%10))
+    s=$((s+r*r*r))
+    n=$((n/10))
+done
+
+if ((s==temp)); then
+    echo "The Number $temp is Armstrong."
+else
+    echo "The Number $temp is Not Armstrong."
+fi
