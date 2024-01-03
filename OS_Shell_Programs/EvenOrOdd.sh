3eb765f51543938e5e6ef50cbd0fe6601c640fd0
Partha1X <nimbus2kilo@gmail.com>
2024-01-03 19:52:55 +0530

    Write a Shell Program to Check Whether a Given No is Even or Odd

---
+++ OS_Shell_Programs/EvenOrOdd.sh
@@ -0,0 +1,12 @@
+: <<COMMENT
+1. Write a Shell Program to Check Whether a Given No is Even or Odd.
+COMMENT
+
+echo -n "Enter a No: "
+read n
+
+if ((n % 2 == 0)); then #if [ $((n % 2)) -eq 0 ];
+    echo "The number is even."
+else
+    echo "The number is odd."
+fi
